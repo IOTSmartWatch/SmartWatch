5637db76ca2eada3a11a65cb0666126ea818de16
Thanh Sơn <nguyenson20304@gmail.com>
2025-08-28 22:50:45 +0700

    Delete include directory

--- include/thingspeak_manager.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-bool ts_send_update(const char* writeKey, long subs, float btc, float eth, const String& rssTitle);
--- include/wifi_manager.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <WiFi.h>
-
-void wifi_connect(const char* ssid, const char* password);
